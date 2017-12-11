a638eb2bb7aefdd5036ee951aa78616da8658841
Victor Frunza <victor.frunza96@gmail.com>
2017-12-10 23:23:08 -0700

    Updated Readme

--- Data/DataStore.py
+++ Data/DataStore.py
@@ -24,15 +24,15 @@ def compile_readme(results):
     readme = open("README.md", "w")
     introduction = "# ProjectEuler\nMy solutions to problems on Project Euler as I finish them. You can learn more about Project Euler here, https://projecteuler.net/about.\nAll of the solutions are timed, with the time of the original version I wrote (usually a very basic brute force approach) and the timings for the most up to date version. Usually I will only include the final version of the problem in my code, but it is all visible on git. If a former solution is particularly interesting, I may decide to leave it in the code, renaming the function to problem1().\nBelow is the execution time for each problem in milliseconds. Timer is used in python and stopwatch is used in C#. each program is run in at least 100 loops, and then the time is divided by the number of loops to get the time of one instance.\nC# is run with the Visual Studio Microsoft Compiler and Python is run in WSL bash using python3.\n\n# Results\n\n"
     readme.write(introduction)
-    readme.write("| Number | Title | C | C++ | C# | D | Java | JavaScript | Python 3 | Rust | R |\n")
-    readme.write("| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |\n")
+    readme.write("| Title | C | C++ | C# | D | Java | JavaScript | Python 3 | Rust | R |\n")
+    readme.write("| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |\n")
 
     first_line = True
     for line in results.data:
         if first_line:
             first_line = False
         else:
-            readme.write("| " + str(line[0]) + " | " + str(line[1]) + " | " + str(line[2]) + " | " + str(line[3]) + " | " + str(line[4]) + " | " + str(line[5]) + " | " + str(line[6]) + " | " + str(line[7]) + " | " + str(line[8]) + " | " + str(line[9]) + " | " + str(line[10]) + " |\n")
+            readme.write("| " + str(line[0]) + ". " + str(line[1]) + " | " + str(line[2]) + " | " + str(line[3]) + " | " + str(line[4]) + " | " + str(line[5]) + " | " + str(line[6]) + " | " + str(line[7]) + " | " + str(line[8]) + " | " + str(line[9]) + " | " + str(line[10]) + " |\n")
 
 def get_problems(results):
     problems = open("problems.csv", "r").readlines()
